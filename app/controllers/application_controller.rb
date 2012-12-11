0b7de88491df5b5a50f843998435e2cd320d5389
uke <iamuke@gmail.com>
2012-12-11 12:53:06 -0700

    Added date & time to sidebar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery
+	protect_from_forgery
+
+ before_filter :dateTimeStamp_function
+
+ def dateTimeStamp_function
+ 	@dateTime = "#{Time.now.strftime('%d %b %Y')} #{Time.now.strftime('%H:%M:%S')}"
+ end
 end
